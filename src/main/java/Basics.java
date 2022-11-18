eafdb4b0e7d3b27b71fb8b7a6a70e0c7441525bc
RaajputSingh <arpitsingh380@hotmail.com>
2022-11-18 06:00:18 +0530

    Primitive Data types

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,5 +1,32 @@
 public class Basics {
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        //Hello World
+        System.out.println("Hello World");
+
+        //Data Types
+        //Primitive Data Types(sorted by bit size)
+
+        //boolean-> stores only true or false
+        boolean bool = true;
+
+        //byte-> stores 1byte of data           || 1byte = 8bits
+        byte b = 10;
+
+        //char-> stores only a single character || char = 2bytes or 16bits
+        char ch = 'A';
+
+        //integer types
+        //int-> stores only integer values, no fractions                 || int = 4bytes or 32bits
+        int num = 10;
+        //long-> stores integer values but larger than int, no fractions || long = 8bytes or 64bits
+        long longNum = 1000200;
+
+        //floating types -> not precise, should not be used for scientific or financial purposes
+        //float-> stores fractional values, but is not precise  || float 4bytes or 32bits
+        float f = 3.14f;
+        //double-> stores fractional values, but is not precise || float 8bytes or 64bits
+        double d = 3.1415;
+
     }
 }
